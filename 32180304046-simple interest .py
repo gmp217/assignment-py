6b32018c626909841f8807dade53830c33b3eabc
gmp217 <66996033+gmp217@users.noreply.github.com>
2020-06-18 18:33:09 +0530

    Add files via upload

---
+++ 32180304046-simple interest .py
@@ -0,0 +1,8 @@
+#prg2
+print('\n2 SIMPLE INTEREST\n')
+P=int(input('Enter value of p '))
+R =int(input('Enter value of r '))
+T = int(input('Enter value of t '))
+# simple interest
+SI = (P * R * T) / 100
+print("simple interest is", SI)
